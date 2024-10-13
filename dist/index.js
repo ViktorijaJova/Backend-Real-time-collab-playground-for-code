0ab3ecf27527e0967cec2a63fb4790018c389230
ViktorijaJova <viktorija@sols.mk>
2024-10-13 16:28:48 +0200

    added in real time colab

--- dist/index.js
+++ dist/index.js
@@ -1,66 +1,59 @@
-import express from 'express';
-import { Pool } from 'pg';
-import dotenv from 'dotenv';
-import http from 'http';
-import { Server } from 'socket.io';
-import cors from 'cors';
-
-dotenv.config();
-
-const app = express();
-const server = http.createServer(app);
-
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const pg_1 = require("pg");
+const dotenv_1 = __importDefault(require("dotenv"));
+const http_1 = __importDefault(require("http"));
+const socket_io_1 = require("socket.io");
+const cors_1 = __importDefault(require("cors"));
+dotenv_1.default.config();
+const app = express_1.default();
+const server = http_1.default.createServer(app);
 // Enable CORS for Express HTTP requests
-app.use(cors({
+app.use(cors_1.default({
     origin: [
-        'http://localhost:3000', // Local development
-        'https://client-real-time-collab-playground-for-code.vercel.app', // Production
+        'http://localhost:3000',
+        'https://client-real-time-collab-playground-for-code.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
     credentials: true,
 }));
-
 // Set up the Socket.io server with CORS configuration
-const io = new Server(server, {
+const io = new socket_io_1.Server(server, {
     cors: {
         origin: [
-            'http://localhost:3000', // Local development
-            'https://client-real-time-collab-playground-for-code.vercel.app', // Production
+            'http://localhost:3000',
+            'https://client-real-time-collab-playground-for-code.vercel.app',
         ],
         methods: ['GET', 'POST'],
     }
 });
-
 // Middleware to parse JSON requests
-app.use(express.json());
-
+app.use(express_1.default.json());
 app.get('/', (req, res) => {
     res.send('Welcome to the Real-time Code Playground!');
 });
-
 // Connect to the database
-const pool = new Pool({
+const pool = new pg_1.Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
         rejectUnauthorized: false,
     },
 });
-
 // WebSocket setup
 io.on('connection', (socket) => {
     console.log(`A user connected: ${socket.id}`);
-
     socket.on('codeChange', (code) => {
         console.log('Received code change:', code); // Log received code
         socket.broadcast.emit('codeChange', code); // Broadcast to all other clients
     });
-
     socket.on('disconnect', () => {
         console.log(`User disconnected: ${socket.id}`);
     });
 });
-
-
 // Start the server
 const PORT = process.env.PORT || 4000;
 server.listen(PORT, () => {
